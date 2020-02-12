401585d1fe28654b806f6a8a81b611e172198764
Parnika <26497907+Ah31@users.noreply.github.com>
2020-02-13 01:21:24 +0530

    Update MADDPG_model.py

--- MADDPG_model.py
+++ MADDPG_model.py
@@ -4,13 +4,6 @@ import torch.nn
 import torch.nn as nn
 import torch.nn.functional as F
 
-
-def hidden_init(layer):
-    fan_in = layer.weight.data.size()[0]
-    lim = 1. / np.sqrt(fan_in)
-    return (-lim, lim)
-
-
 # if bn is done in here then both actor and critic loss are increasing and critic loss is increasing much faster!
 class Actor(nn.Module):
     def __init__(self, state_size, action_size, seed, fc1_units=64, fc2_units=64):
@@ -29,12 +22,6 @@ class Actor(nn.Module):
         self.fc1 = nn.Linear(out_features=fc1_units, in_features=state_size)
         self.fc2 = nn.Linear(out_features=fc2_units, in_features=fc1_units)
         self.fc3 = nn.Linear(out_features=action_size, in_features=fc2_units)
-        # self.reset_parameters()
-
-    def reset_parameters(self):
-        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
-        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
-        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, state):
         """Build an actor (policy) network that maps states -> actions."""
@@ -61,29 +48,15 @@ class Critic(nn.Module):
 
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
-        # self.fcs1 = nn.Linear(out_features=fcs1_units, in_features=(state_size + action_size) * 3)
-        # self.fc2 = nn.Linear(out_features=fc2_units, in_features=fcs1_units)
-        # self.fc2 = nn.Linear(out_features=fcs1_units, in_features=fcs1_units+action_size)
-
         self.fcs1 = nn.Linear(out_features=fcs1_units, in_features = state_size*3)
         self.fc2 = nn.Linear(out_features=fc2_units, in_features=fcs1_units+(action_size*3))
         self.fc3 = nn.Linear(out_features=1, in_features=fc2_units)
-        # self.reset_parameters()
 
-    def reset_parameters(self):
-        self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
-        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
-        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
-
-    def forward(self, states, actions, action):
+    def forward(self, states, actions):
         """Build a critic (value) network that maps (state, corresponding actions) pairs -> Q-values.
         And the overall goal is to maximise the Q value for these states."""
 
-        # x = F.relu(self.fcs1(torch.cat((states, actions), dim=1)))
-        # x = F.relu(self.fc2(torch.cat((x, action), dim=1)))
         xs = F.relu(self.fcs1(states))
         x = torch.cat((xs, actions), dim=1)
-        # x = F.relu(self.fc2(torch.cat((x,action), dim=1)))
-        # x = F.relu(self.fcs1(feed_ip))  # feed_ip shape = (1024,69)
         x = F.relu(self.fc2(x))
         return self.fc3(x)
